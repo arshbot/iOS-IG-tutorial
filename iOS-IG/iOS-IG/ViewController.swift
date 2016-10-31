c85d547465d7090a48fd0cc50f9d5c973b15ec74
HarshaGoli <harshagoli@gmail.com>
2016-10-31 15:05:38 -0400

    Initial commit :tada:

---
+++ iOS-IG/iOS-IG/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-IG
+//
+//  Created by Harsha Goli on 10/31/16.
+//  Copyright © 2016 Harsha Goli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
