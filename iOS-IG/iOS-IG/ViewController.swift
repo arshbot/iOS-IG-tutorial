25c0e1ea8bd3cdc0baec7544baa4ee932e058ae0
HarshaGoli <harshagoli@gmail.com>
2017-02-23 18:01:52 -0500

    Removed all code not covered

--- iOS-IG/iOS-IG/ViewController.swift
+++ iOS-IG/iOS-IG/ViewController.swift
@@ -18,7 +18,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     //Dummy GPS Data. It literally does not matter what is here, just to prove that data can be drawn
     var GPSData:[CLLocation] = []
     let locationManager = CLLocationManager()
-    var mapView: GMSMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,61 +39,18 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
         
         //Defining the mapView object and initing it to the current screen size
-        mapView = GMSMapView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height - tv.bounds.height))
-        mapView.isMyLocationEnabled = true
-        view.addSubview(mapView)
         
         //Settings anchors for the mapView object
-        mapView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor).isActive = true
-        mapView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        mapView.bottomAnchor.constraint(equalTo: tv.topAnchor).isActive = true
         
         // Creates a marker in Sydney for some reason
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
-        marker.map = mapView
-        
-        setupNavbar()
     
     }
     let path = GMSMutablePath()
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        var userLocation:CLLocation = locations[0]
-        
-        self.mapView.camera = GMSCameraPosition.camera(withLatitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude, zoom: 6.0)
-
-        for coord in locations{
-            path.add(CLLocationCoordinate2D(latitude: coord.coordinate.latitude, longitude: coord.coordinate.longitude))
-        }
-        let polyline = GMSPolyline(path: path)
-        
         GPSData.append(contentsOf: locations)
         tv.reloadData()
-    }
-
-    func setupNavbar() {
-        navigationItem.title = "Locations"
-        
-    }
-    
-    func setupViews(mapView: UIView){
-        view.addSubview(mapView)
-        mapView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        mapView.bottomAnchor.constraint(equalTo: tv.topAnchor).isActive = true
-        mapView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        mapView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
-    }
-    
-
-    //Rarely used
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     //Returns number of cells, in this case 10
@@ -114,16 +70,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         cell.Time.text = "\(hour):\(minutes)"
         cell.GPS.text = String(describing: GPSData.reversed()[indexPath.row].coordinate.latitude)
     
-        
-        
         return cell
     }
-    
-    //We'll use this later
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-    }
-    
     
 }
 
